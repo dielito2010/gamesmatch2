31503c397a04d1ae3e45ab8914454806dc5d04a5
DanielAntunesRibeiro <d.a_ribeiro2@hotmail.com>
2022-10-14 03:11:06 -0400

    implementando a classe perfil no models

--- global/matchapp/models.py
+++ global/matchapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class perfil(models.Model):
+    #email = models.ForeignKey(User.email)
+    nome = models.CharField(max_length=255)
+    cpf = models.PositiveIntegerField(primary_key=True)
+    game1 = models.CharField(max_length=255)
+    pontuacao1 = models.PositiveIntegerField()
+    game2 = models.CharField(max_length=255, blank=True, null=True)
+    pontuacao2 = models.PositiveIntegerField(blank=True, null=True)
+    dat_nasc = models.DateField()
+    nickname = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True)
+    cep = models.PositiveIntegerField(blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+    zone = models.CharField(max_length=100)
